6c9a24638c6f68b06f897e6651c8f6b12289f720
Gonzalo Villalta <villalta_gonzalo@hotmail.com>
2022-08-25 16:53:27 -0500

    M4U1/transportes/Backend

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -13,4 +13,30 @@ router.get('/', async function (req, res, next) {
   });
 });
 
+router.get('/Admin/novedades/agregar', (req, res, next) => {
+  res.render('Admin/agregar', {
+    layout:'admin/layout'
+  });
+});
+
+router.post("/Admin/novedades/agregar", async (req, res, next) => {
+  try {
+    if (req.body.nombre != "" && req.body.apellido != "" && req.body.edad != "") {
+      await novedadesModel.insertNovedad(req.body);
+      res.redirect('Admin/novedades')
+    } else {
+      res.render ('Admin/agregar', {
+        layout: 'Admin/layout',
+        error: true, message: 'todos los campos son requeridos'
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    res.render('Admin/agregar', {
+      layout: 'Admin/layout',
+      error: true, message: 'No se cargo la novedad'
+    })
+  }
+})
+
 module.exports = router;
